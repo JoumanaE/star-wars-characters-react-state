32bdc5e3ef0273a17513ded09b9fb34ca77635c3
Joumana El Alaoui <joumana.codes@gmail.com>
2019-12-18 09:24:14 -0600

    Adding useFetch

--- src/index.js
+++ src/index.js
@@ -5,12 +5,81 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import CharacterList from './CharacterList';
 
-import dummyData from './dummy-data';
+// import dummyData from './dummy-data';
 
 import './styles.scss';
+import endpoint from './endpoint';
+
+const initialState = {
+  result: null,
+  loading: true,
+  error: null,
+};
+
+const fetchReducer = (state, action) => {
+  if (action.type === 'LOADING') {
+    return {
+      result: null,
+      loading: true,
+      error: null,
+    };
+  }
+
+  if (action.type === 'RESPONSE_COMPLETE') {
+    return {
+      result: action.payload.response,
+      loading: false,
+      error: null,
+    };
+  }
+
+  if (action.type === 'ERROR') {
+    return {
+      result: null,
+      loading: false,
+      error: action.payload.error,
+    };
+  }
+
+  return state;
+};
+
+const useFetch = url => {
+  const [state, dispatch] = React.useReducer(fetchReducer, initialState);
+
+  React.useEffect(() => {
+    dispatch({ type: 'LOADING' });
+
+    const fetchUrl = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        dispatch({ type: 'RESPONSE_COMPLETE', payload: { response: data } });
+      } catch (error) {
+        dispatch({ type: 'ERROR', payload: { error } });
+      }
+    };
+
+    fetchUrl();
+
+    // fetch(endpoint + '/characters')
+    //   .then(response => response.json())
+    //   .then(response => {
+    //     setLoading(false);
+    //     setResponse(response);
+    //   })
+    //   .catch(error => {
+    //     setLoading(false);
+    //     setError(error);
+    //   });
+  }, []);
+
+  return [state.result, state.loading, state.error];
+};
 
 const Application = () => {
-  const [characters, setCharacters] = useState(dummyData);
+  const [response, loading, error] = useFetch(endpoint + '/characters');
+  const characters = (response && response.characters) || [];
 
   return (
     <div className="Application">
@@ -19,7 +88,12 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
-          <CharacterList characters={characters} />
+          {loading ? (
+            <p>Loading…</p>
+          ) : (
+              <CharacterList characters={characters} />
+            )}
+          {error && <p className="error">{error.message}</p>}
         </section>
       </main>
     </div>
